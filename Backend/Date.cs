a432216e993762d57e6cc7781bf73f9849ff589c
Juan Camilo Mora <camilo2652@gmaill.com>
2026-08-15 01:12:40 -0500

    Date Finish

---
+++ Backend/Date.cs
@@ -0,0 +1,51 @@
+﻿namespace Backend;
+
+public class Date
+{
+    private int _year;
+    private int _month;
+    private int _day;
+
+    public Date()
+    {
+        _year = 1900;
+        _month = 1;
+        _day = 1;
+    }
+
+
+
+
+
+    public Date(int year, int month, int day)
+    {
+        _year = year;
+        _month = month;
+        _day = day;
+    }
+
+
+
+    public int Year
+    {
+        get => _year;
+        set => _year = value;
+    }
+    public int Month
+    {
+        get => _month;
+        set => _month = value;
+    }
+    public int Day
+    {
+        get => _day;
+        set => _day = value;
+    }
+
+
+
+    public override string ToString()
+    {
+        return $"{_year:D4}/{_month:D2}/{_day:D2}";
+    }
+}
---
+++ Frontend/Program.cs
@@ -0,0 +1,16 @@
+﻿using Backend;
+using System.Runtime.CompilerServices;
+
+var date1 = new Date();
+var date2 = new Date(1974, 9, 23);
+var date3 = new Date(2026, 12, 3);
+
+Console.WriteLine(date1);
+Console.WriteLine(date2);
+
+
+
+
+
+
+
