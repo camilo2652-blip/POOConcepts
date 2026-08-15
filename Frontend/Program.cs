2b3e28911c1296ae69300d1f18b1a154711b6ac4
Juan Camilo Mora <camilo2652@gmaill.com>
2026-08-15 01:17:16 -0500

    Correction

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -1,5 +1,5 @@
 ﻿using Backend;
-using System.Runtime.CompilerServices;
+
 
 var date1 = new Date();
 var date2 = new Date(1974, 9, 23);
@@ -7,8 +7,7 @@ var date3 = new Date(2026, 12, 3);
 
 Console.WriteLine(date1);
 Console.WriteLine(date2);
-
-
+Console.WriteLine(date3);
 
 
 
